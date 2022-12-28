cd2f540dda1a8c4c62155fd14da607e6e73d356b
BakaKaito <BakaKaito@DESKTOP-IUII1ND>
2022-12-28 17:34:48 -0500

    Fix

--- scripts.js
+++ scripts.js
@@ -53,7 +53,7 @@ if (endq.length === 2)
 		organization = 'BakaKaito';
 		project = 'MergeBot.NET';
 		projurl = 'https://github.com/BakaKaito/MergeBot.NET';
-		artName = 'SysBot.NET';
+		artName = 'MergeBot.NET';
 	}
 }
 else if (organization===null || project ===null || projurl ===null)
